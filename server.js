f97b4fbcb212358d60c66b1653d334c91ada3658
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-03-19 21:39:39 +0000

    added dependancies next going to  add routes

--- server.js
+++ server.js
@@ -2,3 +2,20 @@ const express     = require('express');
 const mongoose    = require('mongoose');
 const bodyParser  = require('body-parser');
 const cors        = require('cors');
+const app         = express();
+const config      = require('./config/config');
+const routes      = require('./routes/routes');
+const dest        = `${__dirname}/public`
+
+mongoose.connect(config.db);
+
+if(app.get('env') !== 'production') app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded(){extended: true}));
+app.use(express.static(dest));
+
+// app.use('/api', routes);
+// app.get('/*', (req, res) => res.sendFile(`${dest}/index.html`));
+
+app.listen(config.port, () => console.log(`We are ready to go on port: ${config.port}`));
