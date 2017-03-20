30e7700f3d38962eb34e5bc7f35029309b1d88e0
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-03-20 00:26:58 +0000

    finished intial monster schema

--- models/monster.js
+++ models/monster.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const monsterSchema = mongoose.Schema({
       name: { type: String, trim: true, required: true},
-	    cr: { type: Number, trim: true},
+	    cr: { type: Number},
 	    stats: { str: { type: Number}, dex: { type: Number}, con: { type: Number}, int: { type: Number}, cha: { type: Number}, wis: { type: Number}}
 	    armourClass: { total: { type: Number}, touch: { type: Number}, flatFooted: { type: Number}, natural: { type: Number}},
 	    hp: { type: Number},
@@ -13,11 +13,11 @@ const monsterSchema = mongoose.Schema({
 	    immunities: [{type: String, trim: true}],
 	    resistances:[{ resistType: {type: String, trim: true}, resistancePower:{ type: Number}}],
 	    typeOfMonster:{ family: {type: String, trim: true}, species:[{type: String, trim: true}]},
-	    weaknesses: [type: String, trim: true}],
+	    weaknesses: [{type: String, trim: true}],
 	    spellLikeAbilities:[{  name: {type: Number}, description: {type: String, trim: true}, uses: { type: Number}],
-	    // melee  : array of Objects [{   name  : string,   noOfDie  : number   damageDie  : integer}]
-	    // specialAttacks  : array of Objects [{  name  :string, description: text}]
-	    // specialQuality  : array
+	    melee: [{ name: {type: String, trim: true}, noOfDie: {type: Number}, damageDie: {type: Number}}],
+	    specialAttacks: [{name: {type: String, trim: true}, description: {type: String, trim: true}, noOfDie: {type: Number}, damageDie: {type: Number}}],
+	    specialQuality: [{type: String, trim: true}]
 });
 
 module.exports = mongoose.model('Monster', monsterSchema);
