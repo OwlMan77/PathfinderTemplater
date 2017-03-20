d5f6e7918cadd70b8c67fb9541629b056df241d2
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-03-20 00:50:48 +0000

    debugging and added html boilerplate

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser  = require('body-parser');
 const cors        = require('cors');
 const app         = express();
 const config      = require('./config/config');
-const routes      = require('./routes/routes');
+const routes      = require('./config/routes');
 const dest        = `${__dirname}/public`
 
 mongoose.connect(config.db);
@@ -13,10 +13,10 @@ mongoose.connect(config.db);
 if(app.get('env') !== 'production') app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded(){extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(dest));
 
-// app.use('/api', routes);
-// app.get('/*', (req, res) => res.sendFile(`${dest}/index.html`));
+app.use('/api', routes);
+app.get('/*', (req, res) => res.sendFile(`${dest}/index.html`));
 
 app.listen(config.port, () => console.log(`We are ready to go on port: ${config.port}`));
