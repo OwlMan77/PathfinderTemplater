b4d64d9d2c246c5f4b0b4beb2dc197be5ce1900c
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-03-22 10:37:31 +0000

    added monsterIndex to monster controller

--- controllers/monsters.js
+++ controllers/monsters.js
@@ -1,2 +1,12 @@
 module.exports = {
+  index: monsterIndex
+}
+
+const Monster = require('../model/monster');
+
+function monsterIndex(req, res){
+  Monster.find(err, monsters)=> {
+    if(err) return res.status(500).json({message:'Something went wrong displaying monsterIndex.'});
+    return res.status(200).json(monsters);
+  }
 }
