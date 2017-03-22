a6ce4f88e004774ca2315f4f40f877140f3cd14f
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-03-22 12:28:11 +0000

    added comments, added templateCreate to template controller

--- controllers/monsters.js
+++ controllers/monsters.js
@@ -2,11 +2,12 @@ module.exports = {
   index: monsterIndex
 }
 
-const Monster = require('../model/monster');
+const Monster = require('../models/monster');
 
+//will show all monsters unless there is an error, if there is an error it will display the error message
 function monsterIndex(req, res){
-  Monster.find(err, monsters)=> {
+  Monster.find((err, monsters)=> {
     if(err) return res.status(500).json({message:'Something went wrong displaying monsterIndex.'});
     return res.status(200).json(monsters);
-  }
+  });
 }
--- controllers/templates.js
+++ controllers/templates.js
@@ -1,3 +1,23 @@
 module.exports = {
+index: templateIndex
+create: templateCreate
+}
 
+const Template = require('../models/templates')
+
+//will show all templates unless there is an error, if there is an error it will display the error message
+function templateIndex(req, res){
+  Template.find((err, templates) => {
+    if(err) res.status(500).json({message: 'Something went wrong with templateIndex'})
+    return res.status(200).json(templates);
+  });
+}
+
+//will save a new template unless there is an error
+function templateCreate(req, res){
+  const template = new Template(req.body);
+  template.save((err. user) => {
+    if (err) res.status(500).json({message: 'Something went wrong with templateCreate'});
+    return res.status(201).json(template);
+  });
 }
