708ffe5f3f28acf85b12e976a6dbdd7a23b25acc
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-03-24 01:24:22 +0000

    added seeds, changed models to have ranged field

--- db/seeds.js
+++ db/seeds.js
@@ -8,3 +8,46 @@ const Template = require('../models/monster');
 
 Monster.collection.drop();
 Template.collection.drop();
+
+const monsters = [
+  {
+    name: 'Goblin' ,
+    cr: 0.33,
+    stats: { str: 11, dex: 15, con: 12, int: 10, cha: 6 , wis: 9}
+    armourClass: { total: 16, touch: 13, flatFooted: 14, natural: 0},
+    hp: 6,
+    fort: 3,
+    ref:  2,
+    will: –1,
+    dr: { damageReduction: 0, drPierce: '' },
+    immunities: [],
+    resistances:[],
+    typeOfMonster: [{family: 'humanoid', species:'goblinoid'}],
+    weaknesses: [],
+    spellLikeAbilities:[],
+    melee: [{ name: 'shortsword +2', noOfDie: 1, damageDie: 4}],
+    specialAttacks: [],
+    specialQuality: []
+  },
+  {
+    name: 'Kobold' ,
+    cr: 0.33,
+    stats: { str: 11, dex: 15, con: 12, int: 10, cha: 6 , wis: 9}
+    armourClass: { total: 16, touch: 13, flatFooted: 14, natural: 0},
+    hp: 5,
+    fort: 3,
+    ref:  2,
+    will: –1,
+    dr: { damageReduction: 0, drPierce: '' },
+    immunities: [],
+    resistances:[],
+    typeOfMonster: [{family: 'humanoid', species:'reptilian'}],
+    weaknesses: ['light sensitivity'],
+    spellLikeAbilities:[],
+    melee: [{ name: 'spear', noOfDie: 1, damageDie: 6}],
+    ranged: [{ name: {type: String, trim: true}, noOfDie: {type: Number, max: maxNum}, damageDie: {type: Number, max: maxNum}}],
+    specialAttacks: [],
+    specialQuality: []
+  }
+
+];
--- models/template.js
+++ models/template.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
-
+const maxNum = 99;
 const templateSchema = mongoose.Schema({
        name: {type: String, trim: true, required: true},
        typeOfMonster: { family: {type: String, trim: true}, species:[{type: String, trim: true}]},
-       cr:{ type: Number},
-       stats: { str: { type: Number}, dex: { type: Number}, con: { type: Number}, int: { type: Number}, cha: { type: Number}, wis: { type: Number}},
-       acBonus: { type: Number},
-       melee: [{ name: {type: String, trim: true}, noOfDie: {type: Number}, damageDie: {type: Number}}],
- 	     specialAttacks: [{name: {type: String, trim: true}, description: {type: String, trim: true}, noOfDie: {type: Number}, damageDie: {type: Number}}],
+       cr:{ type: Number, max: maxNum},
+       stats: { str: { type: Number, max: maxNum}, dex: { type: Number, max: maxNum}, con: { type: Number, max: maxNum}, int: { type: Number, max: maxNum}, cha: { type: Number, max: maxNum}, wis: { type: Number, max: maxNum}},
+       acBonus: { type: Number, max: maxNum},
+       melee: [{ name: {type: String, trim: true}, noOfDie: {type: Number, max: maxNum}, damageDie: {type: Number, max: maxNum}}],
+       ranged: [{ name: {type: String, trim: true}, noOfDie: {type: Number, max: maxNum}, damageDie: {type: Number, max: maxNum}}],
+ 	     specialAttacks: [{name: {type: String, trim: true}, description: {type: String, trim: true}, noOfDie: {type: Number, max: maxNum}, damageDie: {type: Number, max: maxNum}}],
  	     specialQuality: [{type: String, trim: true}]
    }
 );
