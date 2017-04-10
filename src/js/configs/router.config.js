375499f9e2537d18a115ea26abd49654aecb8c52
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-04-10 18:31:33 +0100

    post-gulp

--- src/js/configs/router.config.js
+++ src/js/configs/router.config.js
@@ -2,7 +2,7 @@ angular
   .module('monster')
   .config(Router);
 
-Router.$inject = ["$stateProvider", "$locationProvider", "$urlProvider"]
+Router.$inject = ["$stateProvider", "$locationProvider", "$urlProvider"];
 function Router($stateProvider, $locationProvider, $urlProvider){
   $locationProvider.html5Mode(true);
   // $stateProvider
