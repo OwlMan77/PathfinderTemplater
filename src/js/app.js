f8466e94cf49045b3a7ec6cd895f2bbf77d5cc35
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-04-10 18:26:00 +0100

    added angular and ui-router

---
+++ src/js/app.js
@@ -0,0 +1,4 @@
+angular
+  .module( 'monster', [
+    'ui-router'
+  ]);
---
+++ src/js/configs/router.config.js
@@ -0,0 +1,9 @@
+angular
+  .module('monster')
+  .config(Router);
+
+Router.$inject = ["$stateProvider", "$locationProvider", "$urlProvider"]
+function Router($stateProvider, $locationProvider, $urlProvider){
+  $locationProvider.html5Mode(true);
+  // $stateProvider
+}
