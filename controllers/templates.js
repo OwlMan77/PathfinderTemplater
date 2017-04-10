10df3adf14d006a18527843f0ffb7d98bdbeffa6
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-04-10 19:33:57 +0100

    added more API routes for templates

--- controllers/templates.js
+++ controllers/templates.js
@@ -1,6 +1,9 @@
 module.exports = {
 index: templateIndex,
-create: templateCreate
+create: templateCreate,
+show: templateShow,
+update: templateUpdate,
+delete: templateDelete
 }
 
 const Template = require('../models/template')
@@ -19,5 +22,27 @@ function templateCreate(req, res){
   template.save((err, user) => {
     if (err) res.status(500).json({message: 'Something went wrong with templateCreate'});
     return res.status(201).json(template);
+  });
+}
+function templateShow(req, res) {
+  Monster.findById(req.params.id, (err, monster) => {
+    if (err) return res.status(500).json({ messsage: 'Something wrong retrieving that spell.' });
+    if (!character) return res.status(404).json({ message: 'No spell by that name resides here. '});
+    return res.status(200).json(character);
+  });
+}
+
+function templateUpdate(req, res) {
+  Monster.findByIdAndUpdate(req.params.id, req.body, (err, monster) => {
+    if (err) return res.status(500).json({ messsage: 'Something went wrong with altering that spell.' });
+    if (!character) return res.status(404).json({ message: 'That spell cannot be altered.'});
+    return res.status(200).json(character);
+  });
+}
+
+function templateDelete(req, res) {
+  Monster.findByIdAndRemove(req.params.id, err => {
+    if (err) return res.status(500).json({ messsage: 'This magic cannot be dispelled.' });
+    return res.sendStatus(204);
   });
 }
